fe5f1390ada9291a727b6d46f0d72653e65c8894
djoe <djoe@example.com>
2026-04-25 22:04:43 +0100

    fix : Moved comparrent in to public and added a foward declaration of the lattice class.

--- agent.h
+++ agent.h
@@ -2,20 +2,23 @@
 #define AGENT_H
 
 #include <random>
+#include <vector>
+
+class Lattice; // Forward declaration of Lattice class
 
 // Define the class agent 
 class Agent {
 private:
     int x, y; // Position of the agent
-    enum Compartment { SUSCEPTIBLE = 0, EXPOSED = 1, INFECTED = 2, RECOVERED = 3 }; // compartment of agent (0 = susceptible, 1 = exposed, 2 = infected, 3 = recovered)
-    Compartment current_state; // current state of the agent
     double sigma; // chance to become infected
     double gamma; // chance to recover
 public:
     Agent(int x, int y, Compartment current_state, double sigma, double gamma); // constructor
+    enum Compartment { SUSCEPTIBLE = 0, EXPOSED = 1, INFECTED = 2, RECOVERED = 3 }; // compartment of agent (0 = susceptible, 1 = exposed, 2 = infected, 3 = recovered)
+    Compartment current_state; // current state of the agent
     void move(); // function to move the agent
     Compartment getCompartment() const { return current_state; }; // function to get the compartment of the agent
-    void updateCompartment(const Agents& other); // function to update the compartment of the agent
+    void updateCompartment(const std::vector<Agent>& other); // function to update the compartment of the agent
     void updatelattice(Lattice& lattice); // function to update the lattice with the new position and compartment of the agent
     int getX() const { return x; } // function to get the x position of the agent
     int getY() const { return y; } // function to get the y position of the agent    
